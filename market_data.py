34f03420d71fc586e04f46b55885665003e5e4de
Mayuresh Saindane <saindanemayuresh069@gmail.com>
2026-04-07 23:10:43 +0530

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -1,31 +1,33 @@
 import yfinance as yf
 
+
 def get_market_data(company):
 
     ticker = company.upper().replace(" ", "") + ".NS"
-    stock = yf.Ticker(ticker)
 
     try:
+        stock = yf.Ticker(ticker)
         hist = stock.history(period="1d")
-        price = hist["Close"].iloc[-1]
 
-        info = stock.info
-        market_cap = info.get("marketCap", 0)
-
-        if price == 0:
+        if hist.empty:
             raise Exception
+
+        price = hist["Close"].iloc[-1]
+        market_cap = stock.info.get("marketCap", 0)
 
     except:
         ticker = company.upper()
         stock = yf.Ticker(ticker)
 
         hist = stock.history(period="1d")
-        price = hist["Close"].iloc[-1]
 
-        info = stock.info
-        market_cap = info.get("marketCap", 0)
+        if hist.empty:
+            return {"price": 0, "market_cap": 0}
+
+        price = hist["Close"].iloc[-1]
+        market_cap = stock.info.get("marketCap", 0)
 
     return {
-        "price": round(price, 2),
-        "market_cap": market_cap
+        "price": float(price),
+        "market_cap": float(market_cap)
     }
