cf09747272c7858c3ec2d18c1c9f13bf67255e5a
Mayuresh Saindane <saindanemayuresh069@gmail.com>
2026-04-08 00:21:01 +0530

    Update valuation.py

--- valuation.py
+++ valuation.py
@@ -1,13 +1,16 @@
 def calculate_dcf(df):
 
-    last_cashflow = df.iloc[-1]["OCF"]
-    growth_rate = 0.10
-    discount_rate = 0.12
+    if df.empty:
+        return 0
+
+    last_ocf = df.iloc[-1]["OCF"]
+
+    growth = 0.1
+    discount = 0.12
 
     value = 0
 
     for i in range(1, 6):
-        future_cf = last_cashflow * ((1 + growth_rate) ** i)
-        value += future_cf / ((1 + discount_rate) ** i)
+        value += (last_ocf * (1 + growth) ** i) / ((1 + discount) ** i)
 
     return value
