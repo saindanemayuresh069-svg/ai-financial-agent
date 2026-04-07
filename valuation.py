a957ec7bf22ef5f346c8c3d620e5505463f96b89
Mayuresh Saindane <saindanemayuresh069@gmail.com>
2026-04-07 22:11:28 +0530

    Update valuation.py

--- valuation.py
+++ valuation.py
@@ -1,16 +1,20 @@
 def calculate_dcf(df, growth_rate=0.12, discount_rate=0.10, terminal_growth=0.04):
 
+    if "OCF" not in df.columns:
+        return 0
+
     latest_ocf = df.iloc[-1]["OCF"]
+
+    if latest_ocf == 0:
+        return 0
 
     cash_flows = []
 
-    # 5-year projection
     for i in range(1, 6):
         cf = latest_ocf * ((1 + growth_rate) ** i)
         discounted_cf = cf / ((1 + discount_rate) ** i)
         cash_flows.append(discounted_cf)
 
-    # Terminal value
     terminal_value = (cash_flows[-1] * (1 + terminal_growth)) / (discount_rate - terminal_growth)
     terminal_discounted = terminal_value / ((1 + discount_rate) ** 5)
 
